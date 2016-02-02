f182eebfe260115de0a8df71290a749d73b99fdc
Djailla <bastien.vallet@gmail.com>
2016-02-02 16:37:41 +0100

    Fix build script

--- com.djailla.linux_dash/build.sh
+++ com.djailla.linux_dash/build.sh
@@ -5,6 +5,7 @@ mkdir -m 755 -p /opt/linux_dash/
 install -m 755 /home/source/rc.local /etc
 
 # Add nodejs repo
+apt-get update
 apt-get install -y -q curl git
 curl -sL https://deb.nodesource.com/setup_5.x | bash -
 
