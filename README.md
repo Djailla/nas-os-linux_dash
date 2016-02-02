20fb7d5b15b5e5e844ecc4b8ac23ad97661c4f68
Djailla <bastien.vallet@gmail.com>
2016-02-02 16:15:48 +0100

    First drop

---
+++ README.md
@@ -0,0 +1,7 @@
+Linux Dash - NAS OS App
+==================================
+
+**Linux Dash** is an app to install on NASes running NAS OS (for Seagate or LaCie NAS).
+It's based on the [Linux Dash](https://github.com/afaqurk/linux-dash) project.
+
+It's a dashboard for NAS monitoring
---
+++ com.djailla.linux_dash/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -m 755 -p /opt/linux_dash/
+
+install -m 755 /home/source/rc.local /etc
+
+# Add nodejs repo
+apt-get install -y -q curl git
+curl -sL https://deb.nodesource.com/setup_5.x | bash -
+
+# Install nodejs components
+apt-get install -y -q nodejs npm
+
+# Checkout sources in /tmp
+cd /opt/linux_dash/
+git clone https://github.com/afaqurk/linux-dash.git
+
+# Checkout last tag
+cd linux-dash
+
+# Install the component
+npm install
+
+exit 0
