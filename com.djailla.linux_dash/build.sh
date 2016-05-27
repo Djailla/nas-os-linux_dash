fba6399e6157e4269eef2efa08c3150428379cca
Djailla <bastien.vallet@gmail.com>
2016-05-27 16:08:16 +0200

    Cleanup build script

--- com.djailla.linux_dash/build.sh
+++ com.djailla.linux_dash/build.sh
@@ -4,12 +4,11 @@ mkdir -m 755 -p /opt/linux_dash/
 
 install -m 755 /home/source/rc.local /etc
 
-# Add nodejs repo
+# Install nodejs components
 apt-get update
 apt-get install -y -q curl git
 if [ $RAINBOW_ARCHITECTURE = "x86_64" ]
 then
-    # Install nodejs components
     curl -sL https://deb.nodesource.com/setup_5.x | bash -
     apt-get install -y -q nodejs npm
 else
@@ -23,5 +22,9 @@ git clone https://github.com/afaqurk/linux-dash.git
 # Install the component
 cd linux-dash
 npm install
+
+# Cleanup
+apt-get autoremove curl git
+apt-get clean
 
 exit 0
