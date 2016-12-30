2cdb7ef5fcb76137b32fe770ed131c1666a33e59
Djailla <bastien.vallet@gmail.com>
2016-12-30 20:14:26 +0100

    Add armv7 support by removing nodejs requirement.

--- com.djailla.linux_dash/build.sh
+++ com.djailla.linux_dash/build.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 
 mkdir -m 755 -p /opt/linux_dash/
-
 install -m 755 /home/source/rc.local /etc
 
 # Install nodejs components
 apt-get update
-apt-get install -y -q curl git
-if [ $RAINBOW_ARCHITECTURE = "x86_64" ]
-then
-    curl -sL https://deb.nodesource.com/setup_6.x | bash -
-    apt-get install -y -q nodejs
-else
-    echo "ARM"
-fi
+apt-get install -y -q git
 
 # Checkout sources in /opt
 cd /opt/linux_dash/
 git clone --depth=1 https://github.com/afaqurk/linux-dash.git
 
-# Install the component
-cd linux-dash
-npm install
-
 # Cleanup
-apt-get -y -q autoremove curl git
+apt-get -y -q autoremove git
 apt-get -y -q clean
 
 exit 0
